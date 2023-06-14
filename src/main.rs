60828bcc80ff8a4ceeb434d7fda3e1f5ce4c259e
FMFigueroa <felixmanuelfigueroa@gmail.com>
2023-06-14 19:46:25 -0300

    higher order functions

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
+// Example 1
+fn foo<F, T>(f: F, x: T) -> T
+where
+    //  Trait bound Fn
+    F: Fn(T) -> T,
+{ 
+    f(x)
+}
+
+// Exmaple 2
+fn map<T, U, F>(collection: &[T], f: F) -> Vec<U>
+where
+    //  Trait bound Fn
+    F: Fn(&T) -> U,
+{
+    collection.iter().map(f).collect()
+}
+
 fn main() {
-    println!("Hello, world!");
+    // Example 1
+    let result = foo(|x| x * x, 2);
+    println!("{}", result);
+
+    // Exmple 2
+    let numbers = vec![1, 2, 3, 4, 5];
+    // Map: Aplicar una función a cada elemento de la colección
+    let squared_numbers: Vec<i32> = map(&numbers, |x| x * x);
+    println!("Squared numbers: {:?}", squared_numbers);
 }
