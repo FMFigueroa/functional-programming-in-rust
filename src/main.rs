c8c24274ecf8434fc6fb2ed6538b8dcbb56d4991
FMFigueroa <felixmanuelfigueroa@gmail.com>
2023-06-21 20:56:02 -0300

    filter &str

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,15 @@
 fn main() {
-    let text = "  ponies  \n   giraffes\niguanas  \nsquid".to_string();
+    let text = " ponies \n  giraffes \n iguanas\nsquid ".to_string();
     let v: Vec<&str> = text.lines().map(str::trim).collect();
+
     assert_eq!(v, ["ponies", "giraffes", "iguanas", "squid"]);
+    println!("{:?}", v);
+
+    let v: Vec<&str> = text
+        .lines()
+        .map(str::trim)
+        .filter(|s| *s != "iguanas")
+        .collect();
+    assert_eq!(v, ["ponies", "giraffes", "squid"]);
+     println!("{:?}", v);
 }
