c9ab394e57b02ca94fcc00aaccfd1353702e9068
FMFigueroa <felixmanuelfigueroa@gmail.com>
2023-06-22 10:00:50 -0300

    Imperative Vs. Functional Programming

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,30 @@
 fn main() {
     let text = " ponies \n  giraffes \n iguanas\nsquid ".to_string();
-    let v: Vec<&str> = text.lines().map(str::trim).collect();
 
-    assert_eq!(v, ["ponies", "giraffes", "iguanas", "squid"]);
+    // Imperative Programming
+    let mut v = vec![];
+    for line in text.lines() {
+        let line = line.trim();
+        if line != "iguanas" {
+            v.push(line);
+        }
+    }
+    assert_eq!(v, ["ponies", "giraffes", "squid"]);
     println!("{:?}", v);
 
-    let v: Vec<&str> = text
+    //========================================================
+    // Functional Programming
+    // Map Adapter
+    let v1: Vec<&str> = text.lines().map(str::trim).collect();
+    assert_eq!(v1, ["ponies", "giraffes", "iguanas", "squid"]);
+    println!("{:?}", v1);
+
+    // Filter Adapter
+    let v2: Vec<&str> = text
         .lines()
         .map(str::trim)
         .filter(|s| *s != "iguanas")
         .collect();
-    assert_eq!(v, ["ponies", "giraffes", "squid"]);
-     println!("{:?}", v);
+    assert_eq!(v2, ["ponies", "giraffes", "squid"]);
+    println!("{:?}", v2);
 }
