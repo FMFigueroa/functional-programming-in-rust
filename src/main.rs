a140c96a45c9eecac0d918830347dc845937514b
FMFigueroa <felixmanuelfigueroa@gmail.com>
2023-06-23 20:27:16 -0300

    flat_map vs flatten

--- src/main.rs
+++ src/main.rs
@@ -74,4 +74,67 @@ fn main() {
     for &city in countries.iter().flat_map(|country| &major_cities[country]) {
         println!("{}", city);
     }
+
+    let tensor = vec![
+        vec![vec![1, 2, 3], vec![4, 5, 6]],
+        vec![vec![7, 8, 9], vec![10, 11, 12]],
+    ];
+
+    let flattened: Vec<i32> = tensor
+        .into_iter()
+        .flat_map(|matrix| matrix.into_iter().flat_map(|row| row.into_iter()))
+        .collect();
+
+    for num in flattened {
+        println!("{}", num);
+    }
+    // BTreeMap adapter
+    use std::collections::BTreeMap;
+
+    // A table mapping cities to their parks: each value is a vector.
+    let mut parks = BTreeMap::new();
+    parks.insert("Portland", vec!["Mt. Tabor Park", "Forest Park"]);
+    parks.insert("Kyoto", vec!["Tadasu-no-Mori Forest", "Maruyama Koen"]);
+    parks.insert("Nashville", vec!["Percy Warner Park", "Dragon Park"]);
+
+    // Build a vector of all parks. `values` gives us an iterator producing
+    // vectors, and then `flatten` produces each vector's elements in turn.
+    let all_parks: Vec<_> = parks.values().flatten().cloned().collect();
+    assert_eq!(
+        all_parks,
+        vec![
+            "Tadasu-no-Mori Forest",
+            "Maruyama Koen",
+            "Percy Warner Park",
+            "Dragon Park",
+            "Mt. Tabor Park",
+            "Forest Park"
+        ]
+    );
+
+    assert_eq!(
+        vec![None, Some("day"), None, Some("one")]
+            .into_iter()
+            .flatten()
+            .collect::<Vec<_>>(),
+        vec!["day", "one"]
+    );
+
+    let name = "hernandez".to_string();
+    let s1 = to_uppercase(&name);
+    let s2 = to_uppercase2(&name);
+    println!("{}", s1);
+    println!("{}", s2);
+}
+
+fn to_uppercase(input: &str) -> String {
+    input
+        .chars()
+        .map(char::to_uppercase)
+        .flatten() // there's a better way
+        .collect()
+}
+
+fn to_uppercase2(input: &str) -> String {
+    input.chars().flat_map(char::to_uppercase).collect()
 }
