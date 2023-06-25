c72f57461bdaebbd263cffd69a77282c8ba3f535
FMFigueroa <felixmanuelfigueroa@gmail.com>
2023-06-25 18:24:55 -0300

    by_ref adapter

--- src/main.rs
+++ src/main.rs
@@ -272,6 +272,22 @@ fn main() {
             ("going", "chicken soup with rice")
         ]
     );
+    // by_ref adapter
+    let message = "To: jimb\r\n\
+    From: id\r\n\
+	\r\n\
+	Oooooh, donuts!!\r\n";
+
+    let mut lines = message.lines();
+    println!("Headers:");
+    for header in lines.by_ref().take_while(|l| !l.is_empty()) {
+        println!("{}", header);
+    }
+    println!("\nBody:");
+    for body in lines {
+        println!("{}", body);
+    }
+    // cloned, copied adapter
 }
 
 // faltten adapter
