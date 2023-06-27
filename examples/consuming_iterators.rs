9e54b738a4bce6644a13deb4960b337b27372a4f
FMFigueroa <felixmanuelfigueroa@gmail.com>
2023-06-27 13:42:36 -0300

    sum and product method

--- examples/consuming_iterators.rs
+++ examples/consuming_iterators.rs
@@ -1,9 +1,23 @@
 fn main() {
-    let mut input = String::new();
+    // count method
+    /* let mut input = String::new();
     loop {
         std::io::stdin()
             .read_line(&mut input)
             .expect("input read error");
         println!("{}", input.lines().count());
-    }
+    } */
+
+    // sum and product method
+    assert_eq!(triangle(20), 210);
+    assert_eq!(factorial(20), 2432902008176640000);
+}
+
+// sum and product method
+fn triangle(n: u64) -> u64 {
+    (1..=n).sum()
+}
+
+fn factorial(n: u64) -> u64 {
+    (1..=n).product()
 }
