0c7e62a3a3aefa5ca9cf6047d2f3a455b7f18ecc
FMFigueroa <felixmanuelfigueroa@gmail.com>
2023-06-27 18:34:58 -0300

    try_fold and try_rfold

---
+++ examples/try_fold.rs
@@ -0,0 +1,15 @@
+use std::{error::Error, io::prelude::*, str::FromStr};
+
+// cmd: cargo watch -q -c -w examples/ -x 'run --example try_fold'
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let stdin = std::io::stdin();
+    let sum = stdin
+        .lock()
+        .lines()
+        .try_fold(0, |sum, line| -> Result<u64, Box<dyn Error>> {
+            Ok(sum + u64::from_str(&line?.trim())?)
+        })?;
+    println!("{}", sum);
+    Ok(())
+}
