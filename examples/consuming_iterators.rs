7fd8bc95811c0d49fb788e9e28c3a646a6d4c2a3
FMFigueroa <felixmanuelfigueroa@gmail.com>
2023-06-27 15:57:55 -0300

    Comparing Item Sequences

--- examples/consuming_iterators.rs
+++ examples/consuming_iterators.rs
@@ -1,3 +1,4 @@
+// cmd: cargo watch -q -c -w examples/ -x 'run --example consuming_iterators'
 fn main() {
     // count method
     /* let mut input = String::new();
@@ -20,8 +21,8 @@ fn main() {
     let numbers = [1.0, 4.0, 2.0];
     assert_eq!(numbers.iter().copied().max_by(cmp), Some(4.0));
     assert_eq!(numbers.iter().copied().min_by(cmp), Some(1.0));
-    let numbers = [1.0, 4.0, NAN, 2.0];
-    assert_eq!(numbers.iter().copied().max_by(cmp), Some(4.0)); // panics
+    // let numbers = [1.0, 4.0, std::f64::NAN, 2.0];
+    // assert_eq!(numbers.iter().copied().max_by(cmp), Some(4.0)); // panics
 
     // max_by_key, min_by_key methods
     use std::collections::HashMap;
@@ -39,8 +40,19 @@ fn main() {
     );
     assert_eq!(
         populations.iter().min_by_key(|&(_name, pop)| pop),
-        Some((&"Greenhorn", &2))
+        Some((&"Boring", &2))
     );
+
+    // Comparing Item Sequences
+    let packed = "Helen of Troy";
+    let spaced = "Helen   of    Troy";
+    let obscure = "Helen of Sandusky"; // nice person, just not famous
+    assert!(packed != spaced);
+    assert!(packed.split_whitespace().eq(spaced.split_whitespace()));
+    // This is true because ' ' < 'o'.
+    assert!(spaced < obscure);
+    // This is true because 'Troy' > 'Sandusky'.
+    assert!(spaced.split_whitespace().gt(obscure.split_whitespace()));
 }
 
 // sum and product methods
@@ -53,7 +65,7 @@ fn factorial(n: u64) -> u64 {
 }
 
 // max_by,  min_by methods
-use std::{cmp::Ordering, f64::NAN};
+use std::cmp::Ordering;
 
 // Compare two f64 values. Panic if given a NaN.
 fn cmp(lhs: &f64, rhs: &f64) -> Ordering {
