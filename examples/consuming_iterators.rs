79e0b6dd56e066da3b9ea24a0480743fcdf16779
FMFigueroa <felixmanuelfigueroa@gmail.com>
2023-06-27 16:33:29 -0300

    rposition and ExactSizeIterator methods

--- examples/consuming_iterators.rs
+++ examples/consuming_iterators.rs
@@ -63,6 +63,11 @@ fn main() {
     let text = "Xerxes";
     assert_eq!(text.chars().position(|c| c == 'e'), Some(1));
     assert_eq!(text.chars().position(|c| c == 'z'), None);
+
+    //rposition
+    let bytes = b"Xerxes";
+    assert_eq!(bytes.iter().rposition(|&c| c == b'e'), Some(4));
+    assert_eq!(bytes.iter().rposition(|&c| c == b'X'), Some(0));
 }
 
 // sum and product methods
