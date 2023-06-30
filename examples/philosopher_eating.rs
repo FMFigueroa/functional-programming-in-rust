17c87848ac005a508f311bf03a2ea640f0366e8d
FMFigueroa <felixmanuelfigueroa@gmail.com>
2023-06-30 02:25:03 -0300

    add other barrier

--- examples/philosopher_eating.rs
+++ examples/philosopher_eating.rs
@@ -8,7 +8,6 @@ use tokio::{
 
 async fn philosopher_eating(name: &str, barrier: Arc<Barrier>, notify: Arc<Notify>) -> () {
     println!("{} está comiendo.", name);
-    //sleep(Duration::from_secs(1)).await; // Simulación de comer
     let wait_result = barrier.wait().await;
     println!("{} ha finalizado de comer.", name);
 
@@ -22,6 +21,7 @@ async fn main() {
     let total_philosopher_eating = 2;
 
     let barrier = Arc::new(Barrier::new(total_philosopher_eating));
+    let barrier_final = Arc::new(Barrier::new(1));
     let notify = Arc::new(Notify::new());
     notify.notify_one();
 
@@ -33,6 +33,8 @@ async fn main() {
         "Michel Foucault",
     ];
 
+    let num_philosophers = philosophers.len();
+
     let mut sorted_philosophers = philosophers.clone();
     sorted_philosophers.sort(); // Ordenar alfabéticamente
 
@@ -40,13 +42,18 @@ async fn main() {
         Vec::new(),
         |mut handles, (i, philosopher)| {
             let philosopher_arc = philosopher;
-            let barrier_arc = barrier.clone();
+            let barrier_arc;
+            if i == num_philosophers - 1 {
+                barrier_arc = barrier_final.clone();
+            } else {
+                barrier_arc = barrier.clone();
+            }
             let notify_arc = notify.clone();
 
             let handle = tokio::spawn(async move {
                 if i % total_philosopher_eating == 0 {
                     notify_arc.notified().await;
-                    sleep(Duration::from_secs(1)).await;
+                    sleep(Duration::from_secs(2)).await;
                 }
                 philosopher_eating(philosopher_arc, barrier_arc, notify_arc).await;
             });
