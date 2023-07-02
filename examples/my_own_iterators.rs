016b5445ab95400d0a83ad70bf76ab1371944808
FMFigueroa <felixmanuelfigueroa@gmail.com>
2023-07-02 11:58:51 -0300

    my_own_iterators

---
+++ examples/my_own_iterators.rs
@@ -0,0 +1,32 @@
+// cmd: cargo watch -q -c -w examples/ -x 'run --example my_own_iterators'
+
+struct I32Range {
+    start: i32,
+    end: i32,
+}
+
+impl Iterator for I32Range {
+    type Item = i32;
+
+    fn next(&mut self) -> Option<i32> {
+        if self.start >= self.end {
+            return None;
+        }
+        let result = Some(self.start);
+        self.start += 1;
+        result
+    }
+}
+fn main() {
+    let mut pi = 0.0;
+    let mut numerator = 1.0;
+
+    for k in (I32Range { start: 0, end: 14 }) {
+        pi += numerator / (2 * k + 1) as f64;
+        numerator /= -3.0;
+    }
+    pi *= f64::sqrt(12.0);
+
+    // IEEE 754 specifies this result exactly.
+    assert_eq!(pi as f32, std::f32::consts::PI);
+}
